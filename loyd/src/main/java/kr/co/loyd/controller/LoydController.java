11a91420772289f6025a57e3b969b23a3fc03e59
lala <silberrook@naver.com>
2021-10-12 20:57:32 +0900

    환경세팅 - hej

---
+++ loyd/src/main/java/kr/co/loyd/controller/LoydController.java
@@ -0,0 +1,33 @@
+package kr.co.loyd.controller;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.loyd.dao.LoydDao;
+import kr.co.loyd.dto.LoydDto;
+
+@Controller
+public class LoydController {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@RequestMapping("/list")
+	public String list(Model model,HttpServletRequest request)
+	{
+		LoydDao ldao=sqlSession.getMapper(LoydDao.class);
+		ArrayList<LoydDto> list=ldao.list();
+		
+		model.addAttribute("list",list);
+		return "/list";
+	}
+	
+	
+}
