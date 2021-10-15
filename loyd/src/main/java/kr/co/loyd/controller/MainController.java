9ae9de721b7f4c7f2b0c31360d37135f6f0161e7
hbh <sout1217@naver.com>
2021-10-15 11:06:16 +0900

    환경설정- hbh

--- loyd/src/main/java/kr/co/loyd/controller/MainController.java
+++ loyd/src/main/java/kr/co/loyd/controller/MainController.java
@@ -1,4 +1,4 @@
-package kr.co.loyd;
+package kr.co.loyd.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
---
+++ loyd/src/main/java/kr/co/loyd/controller/OrderController.java
@@ -0,0 +1,4 @@
+package kr.co.loyd.controller;
+
+public class OrderController {
+}
